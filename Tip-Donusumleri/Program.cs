1a0b7cf4f47a1fffbd70194dd2b52d6dd1b7d128
Veysel Ozer <veyselozer97@outlook.com.tr>
2022-02-22 22:51:53 +0300

    Tip Dönüşümleri İlk Commit

---
+++ Tip-Donusumleri/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace tip_donusumleri
+{
+    class Program
+    {
+            static void Main (string[] args){
+
+                
+
+        // Implicit Conversion  (Bilinçsiz Dönüşüm)
+
+        Console.WriteLine("****Implicit Conversion  (Bilinçsiz Dönüşüm)****");
+
+                byte a  = 5;
+                sbyte b = 30;
+                short c = 10;
+
+                int d = a+b+c;
+                    Console.WriteLine("d : " + d);
+
+                long h = d;
+                    Console.WriteLine("h :" + h);
+
+                float i = h;
+                    Console.WriteLine("i :" + i);
+
+                string e = "Veysel";
+                char f= 'k';
+                object g = e+f+d;
+
+                    Console.WriteLine("g : " + g);
+
+                            
+        // Explicit Conversion  (Biliçli Dönüşüm)
+
+        Console.WriteLine("****Explicit Conversion  (Biliçli Dönüşüm)****");
+
+        int x = 4;
+        byte y = (byte)x;
+        Console.WriteLine("y : " + y);
+
+        int z = 100;
+        byte t = (byte)z;
+        Console.WriteLine("t : " + t);
+
+        float w = 10.3f;
+        byte v = (byte)w;
+        Console.WriteLine("v : " + v);
+
+
+
+        
+
+
+
+
+
+
+
+            
+
+
+            }
+    }
+}
+
+
+
+
