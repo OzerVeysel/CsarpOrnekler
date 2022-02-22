17c778523b6993f4bbee9d9f76296bb9be6a6f1f
Veysel Ozer <veyselozer97@outlook.com.tr>
2022-02-23 00:26:59 +0300

    Hata Yönetimi

---
+++ Hata_Yonetimi/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace hata_yonetimi{
+
+    class Program{
+
+        static void Main(string[] args){
+
+                try
+                {
+                        Console.Write("Bir Sayı Giriniz :");
+                         int sayi = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Girilen Sayi : "+ sayi);
+                }
+
+                catch(Exception ex)
+                {
+                        Console.WriteLine("Hata :" + ex.Message.ToString());
+                }
+
+                finally
+                {
+                        Console.WriteLine("İşlem Tamamlandı.");
+                }        
+                
+
+
+                // *********************************************************
+
+                Console.WriteLine("*******************************");
+
+                try
+                {
+                    //int a = int.Parse(null);
+                    //int a = int.Parse("test");
+                      int a = int.Parse("2000035050505050550");
+                }
+                
+                catch (ArgumentNullException exx)
+                {
+                    Console.WriteLine("Boş Değer Girdiniz");
+                    Console.WriteLine(exx);
+                }
+
+                catch(FormatException exxx)
+                {
+                    Console.WriteLine("Veri Tipi Uygun Değil");
+                    Console.WriteLine(exxx);
+                }
+
+                catch(OverflowException exxxx)
+                {
+                    Console.WriteLine("Çok Küçük veya Çok Büyük Değer Girdiniz :");
+                    Console.WriteLine(exxxx);
+                }
+
+                finally
+                {
+                    Console.WriteLine("İşlem Başarıyla Tamamlandı");
+                }
+
+        }
+
+
+    }
+
+}
+
