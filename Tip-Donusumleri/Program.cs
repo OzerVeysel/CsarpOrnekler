72d87c871bbb94ded9b780ef9c244aab1356a261
Veysel Ozer <veyselozer97@outlook.com.tr>
2022-02-22 22:54:34 +0300

    Tip Dönüşümleri Eksik Kodların Güncellenmesi

--- Tip-Donusumleri/Program.cs
+++ Tip-Donusumleri/Program.cs
@@ -36,30 +36,60 @@ namespace tip_donusumleri
 
         Console.WriteLine("****Explicit Conversion  (Biliçli Dönüşüm)****");
 
-        int x = 4;
-        byte y = (byte)x;
-        Console.WriteLine("y : " + y);
+                int x = 4;
+                byte y = (byte)x;
+                Console.WriteLine("y : " + y);
 
-        int z = 100;
-        byte t = (byte)z;
-        Console.WriteLine("t : " + t);
+                int z = 100;
+                byte t = (byte)z;
+                Console.WriteLine("t : " + t);
 
-        float w = 10.3f;
-        byte v = (byte)w;
-        Console.WriteLine("v : " + v);
+                float w = 10.3f;
+                byte v = (byte)w;
+                Console.WriteLine("v : " + v);
 
+        // *** ToString Methodu ***
 
+        Console.WriteLine("***ToString Methodu***");
 
+                int xx = 6;
+                string yy = xx.ToString();
+                Console.WriteLine("yy :" + yy);
+
+                string zz = 12.5f.ToString();
+                Console.WriteLine("zz :" + zz);
+
+        //System.Convert
         
+        Console.WriteLine("***System.Convert***");
+
+                string s1 ="10",s2="20";
+                int sayi1,sayi2;
+                int toplam;
+
+                sayi1 = Convert.ToInt32(s1);
+                sayi2 = Convert.ToInt32(s2);
+
+                toplam = sayi1 + sayi2;
+
+                Console.WriteLine("Toplam : " + toplam);
 
 
+        // *** Parse ***
 
+        Console.WriteLine("*** Parse ***");
 
+        string metin1 = "10";
+        string metin2 = "10.25";
+        int rakam1 ;
+        double double1;
 
-
+        rakam1 = Int32.Parse(metin1);
+        double1 = Int32.Parse(metin2);
+        Console.WriteLine("rakam1 : " + rakam1);
+        Console.WriteLine("doubel1 : " + double1);
 
             
-
 
             }
     }
