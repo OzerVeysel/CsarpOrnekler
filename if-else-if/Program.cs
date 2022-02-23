473726e820df08f7de0fa387a5b23591fafca253
Veysel Ozer <veyselozer97@outlook.com.tr>
2022-02-23 20:56:20 +0300

    İf Else İf / Ternary

---
+++ if-else-if/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ifelseif
+{
+     class Program
+     {
+
+            static public void Main(string[]args) 
+            {
+                
+                //********* İf - Else - İf **********
+
+                int time = DateTime.Now.Hour;
+
+                if (time >= 6 && time < 11)
+                    Console.WriteLine("Günaydın");
+                else if ( time <= 18)
+                    Console.WriteLine("İyi Günler!");
+                else
+                    Console.WriteLine("İyi Geceler!");
+
+            // ************ Ternary *****************
+
+            string zaman = time <= 18 ? "İyi Günler" : "İyi Geceler";
+
+            zaman = time >= 6 && time < 11 ? "Günaydın" : time <= 18 ? "İyi Günler" : "İyi Geceler";
+
+            Console.WriteLine(zaman);
+
+        }
+     }
+}
+
+    
+;
