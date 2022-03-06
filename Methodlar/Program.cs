c19cdedb38b14086d456d99fb29c2d2e2c7bed38
Veysel Ozer <veyselozer97@outlook.com.tr>
2022-03-06 20:55:40 +0300

    Method Tanımlama

---
+++ Methodlar/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace metotlar
+{
+    class Prgram
+    {
+        static void Main(string[] args)
+        {
+             int a=2;
+             int b=3;
+
+             Console.WriteLine(a+b);
+
+            int sonuc = toplam(2,3);
+            Console.WriteLine(sonuc);
+
+            Methodlar ornek = new Methodlar();
+            ornek.EkranaYazdır(Convert.ToString(sonuc));
+
+         
+        }
+            static int toplam(int deger1,int deger2)
+             {
+                 return (deger1+deger2);
+             }
+    }
+
+    class Methodlar
+    {
+        public void EkranaYazdır(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+    }
+}
