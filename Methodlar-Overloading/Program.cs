47bab28c7ce31719f606066f8ba1bd9c84cafecd
Veysel Ozer <veyselozer97@outlook.com.tr>
2022-03-10 22:47:20 +0300

    Methodlar-Overloading

---
+++ Methodlar-Overloading/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace methodlaroverloading
+{
+    class Prgram
+    {
+        static void Main(string[] args)
+        {
+             string sayi = "999";
+             int outSayi;
+
+            bool sonuc =  int.TryParse(sayi, out outSayi);
+            if(sonuc)
+            {
+                Console.WriteLine("Başarılı");
+                Console.WriteLine(outSayi);
+            }
+            else
+            {
+                Console.WriteLine("Başarısız");
+            }
+
+            Metotlar instance = new Metotlar();
+            instance.Topla(2,6, out int toplamsonuc);
+            Console.WriteLine(toplamsonuc);
+        }
+    }
+
+    class Metotlar
+    {
+        public void Topla(int a,int b, out int toplam)
+        {
+            toplam = a+b;
+
+        }
+    }
+}
--- Methodlar/Program.cs
+++ Methodlar/Program.cs
@@ -4,7 +4,7 @@ namespace metotlar
 {
     class Prgram
     {
-        static void Main(string[] args)
+        static async void Main(string[] args)
         {
              int a=2;
              int b=3;
@@ -17,7 +17,10 @@ namespace metotlar
             Methodlar ornek = new Methodlar();
             ornek.EkranaYazdır(Convert.ToString(sonuc));
 
-         
+            
+            int sonuc2 = ornek.ArttırVeTopla(ref a ,ref b);
+            ornek.EkranaYazdır(Convert.ToString(sonuc2));
+            ornek.EkranaYazdır(Convert.ToString( a+b));
         }
             static int toplam(int deger1,int deger2)
              {
@@ -30,6 +33,13 @@ namespace metotlar
         public void EkranaYazdır(string veri)
         {
             Console.WriteLine(veri);
+        }
+
+        public int ArttırVeTopla(ref int deger1,ref int deger2)
+        {
+            deger1 +=1;
+            deger2 +=1;
+            return deger1 + deger2;
         }
     }
 }
