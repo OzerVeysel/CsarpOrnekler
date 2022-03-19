683255eda91cdf86433bd89789a96e975f370118
Veysel Ozer <veyselozer97@outlook.com.tr>
2022-03-20 01:00:30 +0300

    Datetime ve Math Sınıfları

---
+++ DatetimeveMathSınıfları/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DatetimeveMathSınıfları
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(DateTime.Now.Date);
+            Console.WriteLine(DateTime.Now.Day);
+            Console.WriteLine(DateTime.Now.Month);
+            Console.WriteLine(DateTime.Now.Year);
+            Console.WriteLine(DateTime.Now.Hour);
+            Console.WriteLine(DateTime.Now.Minute);
+            Console.WriteLine(DateTime.Now.Second);
+
+            Console.WriteLine(DateTime.Now.DayOfWeek);
+            Console.WriteLine(DateTime.Now.DayOfYear);
+        }
+    }
+}
